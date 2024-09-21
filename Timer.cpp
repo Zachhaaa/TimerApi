27901998c528fa20c71f8be8468cc014c5dd0201
Zach Puglia <zacharyjustis3@gmail.com>
2024-09-21 11:27:49 -0700

    Scoped Timer + fp precision
    ScopedTimer api added
    getTime() and sleepFor now use floats instead of doubles

--- Timer.cpp
+++ Timer.cpp
@@ -4,9 +4,9 @@ using namespace std::chrono;
 
 high_resolution_clock::time_point appStartTime = high_resolution_clock::now();
 
-double getTime() {
+float getTime() {
 
 	high_resolution_clock::time_point now = high_resolution_clock::now();
-	return duration<double, std::chrono::seconds::period>(now - appStartTime).count(); 
+	return duration<float, std::chrono::seconds::period>(now - appStartTime).count(); 
 
 }
--- Timer.hpp
+++ Timer.hpp
@@ -12,5 +12,28 @@
 */
 
 // @return seconds since the start of the program.
-double getTime();
-inline void sleepFor(double seconds) { if(seconds <= 0.0) return; std::this_thread::sleep_for(std::chrono::nanoseconds(uint64_t(seconds * 1e9))); }
+float getTime();
+inline void sleepFor(float seconds) { if(seconds <= 0.0) return; std::this_thread::sleep_for(std::chrono::nanoseconds(uint64_t(seconds * 1e9))); }
+
+class _ScopedTimer {
+public:
+
+	_ScopedTimer(float* pTimeVariable) : elapsedTime(pTimeVariable) { startTime = getTime(); }
+	~_ScopedTimer() { *elapsedTime = getTime() - startTime; }
+
+private:
+
+	float* elapsedTime;
+	float  startTime;
+
+};
+
+#ifndef DISABLE_SCOPEDTIMER
+
+#define scopedTimer(id, pTimeVariable) _ScopedTimer id(pTimeVariable)
+
+#else 
+
+#define scopedTimer(id, pTimeVariable) ((void)0)
+
+#endif
